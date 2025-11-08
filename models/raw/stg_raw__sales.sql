87da90b2d39bf76df1c8f240553808aafea4e1b8
Şilangül Ka <silan_kahraman@hotmail.com>
2025-11-08 21:15:09 +0000

    ..

--- models/raw/stg_raw__sales.sql
+++
@@ -1,8 +0,0 @@
-with
-
-    source as (select * from `valued-context-474807-t7`.`gz_raw_data`.`raw_gz_sales`),
-
-    renamed as (select date_date, orders_id, pdt_id, revenue, quantity from source)
-
-select *
-from renamed
